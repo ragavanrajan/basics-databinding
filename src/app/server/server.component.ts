53f5480f47697511aa9d68adc6cd38268ea19408
Ragavan Rajan <ragavanrajan@outlook.com>
2017-12-24 08:11:47 +1300

    Changed color

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -17,7 +17,7 @@ export class ServerComponent {
   }
   getColor() {
 
-    return this.serverStatus = 'online' ? 'green' : 'red';
+    return this.serverStatus = 'online' ? 'green' : 'yellow';
 
   }
 }
